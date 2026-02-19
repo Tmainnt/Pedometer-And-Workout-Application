7657f848de89eb45eb3d1c945e6ad29ba2a0046f
Tmainnt <gamerthann@gmail.com>
2026-02-19 16:21:22 +0700

    Icon Text theme

---
+++ lib/theme/font_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FontColor {
+  Color errorColor() {
+    return Color.fromARGB(0, 255, 22, 22);
+  }
+
+  Color generalTextBrightTheme() {
+    return Colors.black;
+  }
+
+  Color postText() {
+    return Color.fromARGB(0, 158, 158, 158);
+  }
+}
---
+++ lib/theme/widget_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WidgetColors {
+  Color boxShadowColor() {
+    return const Color.fromARGB(0, 158, 158, 158);
+  }
+
+  Color iconColor() {
+    return const Color.fromARGB(0, 158, 158, 158);
+  }
+}
