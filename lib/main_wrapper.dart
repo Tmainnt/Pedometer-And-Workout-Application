fccd646e6d6f9b4c22923aaf1b55a7229d1bf1dd
Tmainnt <gamerthann@gmail.com>
2026-02-25 00:17:31 +0700

    Update Page 4

--- lib/main_wrapper.dart
+++ lib/main_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer_application/home_page.dart';
 import 'package:pedometer_application/widget/navbar/buttom_navbar.dart';
+import 'package:pedometer_application/community_page.dart';
 
 class MainWrapper extends StatefulWidget {
   const MainWrapper({super.key});
@@ -16,21 +17,18 @@ class _MainWrapperState extends State<MainWrapper> {
     const HomePage(),
     const Center(child: Text("หน้า รายงาน")),
     const Center(child: Text("หน้า ท่าฝึก")),
-    const Center(child: Text("หน้า ชุมชน")),
+    const CommunityPage(),
     const Center(child: Text("หน้า ห้องสนทนา")),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
+      body: IndexedStack(index: _selectedIndex, children: _pages),
       bottomNavigationBar: BottomNavbar(
-          currentIndex: _selectedIndex,
-          onTap: (index) => setState(() => _selectedIndex = index),
-       ),
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+      ),
     );
   }
 }
