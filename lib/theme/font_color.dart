2687b0c15f45b686e155eff8dc02983680ce93c3
Tmainnt <gamerthann@gmail.com>
2026-02-25 00:21:47 +0700

    Change A from 0 to 255

--- lib/theme/font_color.dart
+++ lib/theme/font_color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class FontColor {
   Color errorColor() {
-    return Color.fromARGB(0, 255, 22, 22);
+    return Color.fromARGB(255, 255, 22, 22);
   }
 
   Color generalTextBrightTheme() {
@@ -10,6 +10,10 @@ class FontColor {
   }
 
   Color postText() {
-    return Color.fromARGB(0, 158, 158, 158);
+    return Color.fromARGB(255, 158, 158, 158);
+  }
+
+  Color postTextWithIcon() {
+    return Color.fromARGB(255, 90, 90, 100);
   }
 }
