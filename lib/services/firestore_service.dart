a73365396f8e0264a983dbe84c2b07338c24813f
Tmainnt <gamerthann@gmail.com>
2026-02-25 00:20:14 +0700

    Add CRUD method

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pedometer_application/models/post.dart';
 
 class FirestoreService {
-  /*final CollectionReference user = FirebaseFirestore.instance.collection(
-    'users',
-  );*/
+  final CollectionReference usersCollection = FirebaseFirestore.instance
+      .collection('users');
+
+  final CollectionReference postCollection = FirebaseFirestore.instance
+      .collection('posts');
 
   Stream<DocumentSnapshot> getUserData() {
     return FirebaseFirestore.instance
@@ -14,23 +17,103 @@ class FirestoreService {
         .snapshots();
   }
 
+  final uid = FirebaseAuth.instance.currentUser!.uid;
+
   Stream<DocumentSnapshot> getUserDataByUID(String UID) {
     return FirebaseFirestore.instance.collection('users').doc(UID).snapshots();
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getPostData() {
-    return FirebaseFirestore.instance.collection('posts').snapshots();
+    return FirebaseFirestore.instance
+        .collection('posts')
+        .orderBy('create_timestamp', descending: true)
+        .snapshots();
   }
 
   dynamic checkHasData(AsyncSnapshot snapshot) {
     if (snapshot.hasError) {
       return Center(child: Text("เกิดข้อผิดพลาดในการอ่านข้อมูล"));
     }
+
     if (snapshot.connectionState == ConnectionState.waiting) {
-      return CircularProgressIndicator(
-        color: const Color.fromARGB(0, 255, 203, 114),
-      );
+      return const Center(child: CircularProgressIndicator());
     }
+
+    if (!snapshot.hasData || snapshot.data == null) {
+      return const Center(child: Text('No data.'));
+    }
+
     return true;
+  }
+
+  Future<void> deletePost(Post post) {
+    return postCollection.doc(post.postID).delete();
+  }
+
+  Future<void> updatePost(Post post) {
+    return postCollection.doc(post.postID).update({
+      'content': post.content,
+      'feeling': post.feeling,
+      'imageUrl': post.imageUrl,
+      'postBy_UID': post.UID,
+      'create_timestamp': post.timestamp,
+      'update_timestamp': post.updateTimestamp,
+    });
+  }
+
+  Stream<bool> hasUserLikedPost(String postID, String uid) {
+    return FirebaseFirestore.instance
+        .collection('posts')
+        .doc(postID)
+        .collection('postLiked')
+        .doc(uid)
+        .snapshots()
+        .map((doc) => doc.exists);
+  }
+
+  Future<void> likePost(String postID) async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    await postCollection.doc(postID).collection('postLiked').doc(uid).set({
+      'liked_at': FieldValue.serverTimestamp(),
+    });
+
+    await postCollection.doc(postID).update({
+      'total_like': FieldValue.increment(1),
+    });
+  }
+
+  Future<void> unlikePost(String postID) async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    await postCollection.doc(postID).collection('postLiked').doc(uid).delete();
+
+    await postCollection.doc(postID).update({
+      'total_like': FieldValue.increment(-1),
+    });
+  }
+
+  Future<void> addComment(String postID, String content) async {
+    await postCollection.doc(postID).collection('postComment').add({
+      'UID': uid,
+      'content': content,
+    });
+
+    await postCollection.doc(postID).update({
+      'total_comment': FieldValue.increment(1),
+    });
+  }
+
+  Future<void> newPost(Post post) async {
+    await postCollection.add({
+      'content': post.content,
+      'create_timestamp': FieldValue.serverTimestamp(),
+      'feeling': post.feeling,
+      'imageUrl': post.imageUrl,
+      'postBy_UID': post.UID,
+      'total_comment': 0,
+      'total_like': 0,
+      'update_timestamp': FieldValue.serverTimestamp(),
+    });
   }
 }
