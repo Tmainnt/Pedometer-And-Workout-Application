5ba4d60f5b6c9eb9cb7126eed58b2282937da8e0
Tmainnt <gamerthann@gmail.com>
2026-02-25 00:23:42 +0700

    Extension to display Numberformatt

---
+++ lib/extension/number_format.dart
@@ -0,0 +1,42 @@
+extension NumberFormatter on int {
+  String get formatCount {
+    if (this >= 1000000000000) {
+      double value = this / 1000000000000;
+      value = (value * 10).truncateToDouble() / 10;
+      String formatted = value.toStringAsFixed(1);
+      formatted = formatted.replaceFirst(RegExp(r'\.0$'), '');
+
+      return '${formatted}T';
+    }
+
+    if (this >= 1000000000) {
+      double value = this / 1000000000;
+      value = (value * 10).truncateToDouble() / 10;
+      String formatted = value.toStringAsFixed(1);
+      formatted = formatted.replaceFirst(RegExp(r'\.0$'), '');
+
+      return '${formatted}B';
+    }
+
+    if (this >= 1000000) {
+      double value = this / 1000000;
+      value = (value * 10).truncateToDouble() / 10;
+      if ((value * 10) % 10 == 0) return '${value}M';
+      String formatted = value.toStringAsFixed(1);
+      formatted = formatted.replaceFirst(RegExp(r'\.0$'), '');
+
+      return '${formatted}M';
+    }
+
+    if (this >= 1000) {
+      double value = this / 1000;
+      value = (value * 10).truncateToDouble() / 10;
+      String formatted = value.toStringAsFixed(1);
+      formatted = formatted.replaceFirst(RegExp(r'\.0$'), '');
+
+      return '${formatted}K';
+    }
+
+    return toString();
+  }
+}
