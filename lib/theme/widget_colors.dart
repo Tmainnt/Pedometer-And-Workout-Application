b1b749d2da9ea5d8bd1207be0f50ec076c829ded
Tmainnt <gamerthann@gmail.com>
2026-02-25 00:22:24 +0700

    Change A from 0 to 255. Add new mrthod call iconColorMoreDark and favoriteIcon

--- lib/theme/widget_colors.dart
+++ lib/theme/widget_colors.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 class WidgetColors {
   Color boxShadowColor() {
-    return const Color.fromARGB(0, 158, 158, 158);
+    return const Color.fromARGB(255, 158, 158, 158);
   }
 
   Color iconColor() {
-    return const Color.fromARGB(0, 158, 158, 158);
+    return const Color.fromARGB(255, 158, 158, 158);
+  }
+
+  Color iconColorMoreDark() {
+    return const Color.fromARGB(255, 90, 90, 100);
+  }
+
+  Color favoriteIcon() {
+    return Colors.red;
   }
 }
