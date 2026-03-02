9a115c8bd44ee4184aaa4b97e9081d2e89750770
Tmainnt <gamerthann@gmail.com>
2026-03-02 16:41:24 +0700

    Update add photo, TextField, navigate to Feeling Select Page and AlertDialog when navigator.pop(context) while textEditingController.text != null and _image != null

--- lib/new_post.dart
+++ lib/new_post.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pedometer_application/community_page.dart';
+import 'package:pedometer_application/feeling_selected.dart';
+import 'package:pedometer_application/theme/font_color.dart';
 import 'package:pedometer_application/theme/widget_colors.dart';
+import 'dart:io';
 
 class NewPost extends StatefulWidget {
   final Map<String, dynamic> userData;
@@ -11,6 +16,55 @@ class NewPost extends StatefulWidget {
 
 class NewPostState extends State<NewPost> {
   final TextEditingController _textEditingController = TextEditingController();
+  final WidgetColors widgetColors = WidgetColors();
+  final FontColor fontColors = FontColor();
+  String? _feeling;
+  // Image Picker section
+  File? _image;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: source,
+      imageQuality: 80,
+      maxWidth: 1024,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _showImageSourceActionSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) => SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.photo),
+              title: const Text('เลือกจาก Gallery'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.camera_alt),
+              title: const Text('ถ่ายภาพ'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,31 +88,49 @@ class NewPostState extends State<NewPost> {
             ),
           ),
         ),
-        leading: /*Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                color: WidgetColors().boxShadowColor(),
-                offset: Offset(0, 3),
-                blurRadius: 4,
-              ),
-            ],
-          ),
-          child: IconButton(
-            onPressed: () {
+        leading: IconButton(
+          onPressed: () {
+            if ((_textEditingController.text).isNotEmpty ||
+                _image != null ||
+                _feeling != null) {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text(
+                    'ยกเลิกการเปลี่ยนแปลงหรือไม่',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  content: Text(
+                    'หากกดยืนยันการเปลี่ยนแปลงทั้งหมดจะสูญหายไป ท่านตัดสินใจจะกดยกเลิกหรือไม่',
+                    style: TextStyle(color: fontColors.textDark()),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        _textEditingController.clear();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CommunityPage(),
+                          ),
+                        );
+                      },
+                      style: TextButton.styleFrom(foregroundColor: Colors.red),
+                      child: const Text('ยืนยัน'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('ยกเลิก'),
+                    ),
+                  ],
+                ),
+              );
+            } else {
               Navigator.pop(context);
-            },
-            padding: EdgeInsets.zero,
-            constraints: BoxConstraints(minWidth: 36, minHeight: 36),
-            icon: Icon(Icons.close, color: WidgetColors().iconColorMoreDark()),
-            style: IconButton.styleFrom(
-              backgroundColor: Colors.white,
-              shape: CircleBorder(),
-            ),
-          ),
-        ),*/ IconButton(
-          onPressed: () => Navigator.pop(context),
+            }
+          },
           icon: Icon(
             Icons.cancel,
             color: Colors.white,
@@ -76,11 +148,39 @@ class NewPostState extends State<NewPost> {
       body: Column(
         children: [
           Container(
-            height: 100,
+            height: 90,
             padding: EdgeInsets.all(20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [],
+              children: [
+                if (widget.userData['user_photoUrl'].isNotEmpty)
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      widget.userData['user_photoUrl'],
+                    ),
+                  ),
+                if (widget.userData['user_photoUrl'].isEmpty)
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/default_profile.png'),
+                  ),
+                SizedBox(width: 20),
+                if (_feeling == null)
+                  Text(
+                    widget.userData['user_name'],
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )
+                else
+                  Column(
+                    children: [
+                      Text(widget.userData['user_name']),
+                      Row(
+                        children: [
+                          Text('ตอนนี้กำลังรู้สึก'),
+                          Image.asset('assets/emotion/$_feeling'),
+                        ],
+                      ),
+                    ],
+                  ),
+              ],
             ),
           ),
           Divider(height: 3, color: WidgetColors().iconColor(), thickness: 1),
@@ -90,21 +190,27 @@ class NewPostState extends State<NewPost> {
                 TextField(
                   controller: _textEditingController,
                   decoration: InputDecoration(
+                    border: InputBorder.none,
                     contentPadding: EdgeInsets.fromLTRB(10, 25, 0, 0),
                     hintText: 'วันนี้คุณเป็นยังไงบ้าง ระบายให้ฟังได้นะ...',
                   ),
+                  keyboardType: TextInputType.multiline,
+                  minLines: 1,
+                  maxLines: null,
+                  textInputAction: TextInputAction.newline,
                 ),
-                /*Image(
-                  image: image,
-                ),*/
-                // แสดงรูปจากการที่ผู้ใช้เลือกรูปภาพมา เดี๋ยวมาทำต่อนะจ๊ะ
+                SizedBox(height: 15),
+                if (_image != null) Image.file(_image!),
               ],
             ),
           ),
         ],
       ),
-
       bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        elevation: 0,
+        height: 110,
         child: Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
@@ -124,29 +230,88 @@ class NewPostState extends State<NewPost> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: WidgetColors().boxShadowColor(),
-                          offset: Offset(0, 0),
-                          blurRadius: 5,
-                        ),
-                      ],
+                  GestureDetector(
+                    onTap: () {
+                      _showImageSourceActionSheet();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: WidgetColors().boxShadowColor(),
+                            offset: Offset(0, 0),
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Icon(
+                              Icons.add_photo_alternate_outlined,
+                              color: widgetColors.iconColorMoreDark(),
+                              size: 40,
+                            ),
+                          ),
+                          Text(
+                            'เพิ่มรูปภาพ',
+                            style: TextStyle(
+                              color: fontColors.textWithIcon(),
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    child: Row(children: [
-
-                    ],
                   ),
+                  SizedBox(width: 20),
+                  GestureDetector(
+                    onTap: () {
+                      _goToFeelingPage();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: WidgetColors().boxShadowColor(),
+                            offset: Offset(0, 0),
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Icon(
+                              Icons.emoji_emotions_outlined,
+                              color: widgetColors.iconColorMoreDark(),
+                              size: 40,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'ความรู้สึก',
+                            style: TextStyle(
+                              color: fontColors.textWithIcon(),
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 10),
-                  Container(),
                 ],
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(10),
+                    borderRadius: BorderRadiusGeometry.circular(15),
                   ),
                   backgroundColor: WidgetColors().confirmButton(),
                 ),
@@ -155,7 +320,7 @@ class NewPostState extends State<NewPost> {
                 },
                 child: Text(
                   'บันทึก',
-                  style: TextStyle(fontSize: 11, color: Colors.white),
+                  style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
               ),
             ],
@@ -163,5 +328,16 @@ class NewPostState extends State<NewPost> {
         ),
       ),
     );
+  }
+
+  void _goToFeelingPage() async {
+    final feeling = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FeelingSelected()),
+    );
+
+    if (feeling != null) {
+      _feeling = feeling;
+    }
   }
 }
