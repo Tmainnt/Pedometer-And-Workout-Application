a15e8e3607c4141bd805d1dc83607ce11dcdecc9
Tmainnt <gamerthann@gmail.com>
2026-03-05 19:43:21 +0700

    remove set method, add field emotionUrl and feeling

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,33 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:pedometer_application/services/firestore_service.dart';
 
 class Post {
   final String? _postID;
   final String _UID;
   String _content;
-  String _feeling;
-  String _imageUrl;
+  String? _feeling;
+  String? _emotionUrl;
+  String? _imageUrl;
   int _totalLike;
   int _totalComment;
   DateTime _timestamp; // เวลาที่โพสต์ถูกสร้างครั้งแรก เก็บแยกกับการแก้ไข
-  DateTime _updateTimestamp; // เวลาที่โพสต์ถูกแก้ไข เวลาตรงนี้จะเปลี่ยน
-  FirestoreService firestoreService = FirestoreService();
+  DateTime
+  _updateTimestamp; // เวลาที่โพสต์ถูกแก้ไข เวลาตรงนี้จะเปลี่ยน เอาไว้สำหรับให้ admin เช็คเวลาการเปลี่ยนแปลงของโพสต์ล่าสุด
 
   Post({
     required String postID,
     required String UID,
-    required String content,
-    required String feeling,
-    required String imageUrl,
+    required String? content,
+    required String? feeling,
+    required String? emotionUrl,
+    required String? imageUrl,
     required int totalLike,
     required int totalComment,
     required DateTime timestamp,
     required DateTime updateTimestamp,
   }) : _postID = postID,
        _UID = UID,
-       _content = content,
-       _feeling = feeling,
-       _imageUrl = imageUrl,
+       _content = content ?? '',
+       _feeling = feeling ?? '',
+       _emotionUrl = emotionUrl ?? '',
+       _imageUrl = imageUrl ?? '',
        _totalLike = totalLike,
        _totalComment = totalComment,
        _timestamp = timestamp,
@@ -41,6 +43,7 @@ class Post {
       UID: data['postBy_UID'] ?? '',
       content: data['content'] ?? '',
       feeling: data['feeling'] ?? '',
+      emotionUrl: data['emotionUrl'] ?? '',
       imageUrl: data['imageUrl'] ?? '',
       totalLike: data['total_like'] ?? 0,
       totalComment: data['total_comment'] ?? 0,
@@ -56,30 +59,11 @@ class Post {
   String? get postID => _postID;
   String get UID => _UID;
   String get content => _content;
-  String get feeling => _feeling;
-  String get imageUrl => _imageUrl;
+  String? get feeling => _feeling;
+  String? get emotionUrl => _emotionUrl;
+  String? get imageUrl => _imageUrl;
   int get totalLike => _totalLike;
   int get totalComment => _totalComment;
   DateTime get timestamp => _timestamp;
   DateTime get updateTimestamp => _updateTimestamp;
-
-  void updateWhenEdit() {
-    _updateTimestamp = DateTime.now();
-    firestoreService.updatePost(this);
-  }
-
-  set updateContent(String newContent) {
-    _content = newContent;
-    updateWhenEdit();
-  }
-
-  set updateFeeling(String feeling) {
-    _feeling = feeling;
-    updateWhenEdit();
-  }
-
-  set updateImageUrl(String url) {
-    _imageUrl = url;
-    updateWhenEdit();
-  }
 }
