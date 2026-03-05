8f418b66d6b7b2999de47aac246cc56ae44fc507
Tmainnt <gamerthann@gmail.com>
2026-03-05 19:47:32 +0700

    adjust layout and new method call confirmText and discardText

--- lib/theme/font_color.dart
+++ lib/theme/font_color.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 
 class FontColor {
-  Color errorColor() {
-    return Color.fromARGB(255, 255, 22, 22);
-  }
+  Color errorColor() => Color.fromARGB(255, 255, 22, 22);
 
-  Color generalTextBrightTheme() {
-    return Colors.black;
-  }
+  Color generalTextBrightTheme() => Colors.black;
 
-  Color postText() {
-    return Color.fromARGB(255, 158, 158, 158);
-  }
+  Color postText() => Color.fromARGB(255, 158, 158, 158);
 
-  Color textWithIcon() {
-    return Color.fromARGB(255, 90, 90, 100);
-  }
+  Color textWithIcon() => Color.fromARGB(255, 90, 90, 100);
 
-  Color tTextDark() {
-    return Color.fromARGB(255, 90, 90, 100);
-  }
+  Color textDark() => Color.fromARGB(255, 90, 90, 100);
+
+  Color confirmText() => Colors.blueAccent;
+
+  Color discardText() => Colors.red;
 }
