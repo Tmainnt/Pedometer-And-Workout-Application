695c381e021844fd7eb33924c99d0859a41ee435
Tmainnt <gamerthann@gmail.com>
2026-03-05 19:48:43 +0700

    adjust layout

--- lib/theme/widget_colors.dart
+++ lib/theme/widget_colors.dart
@@ -1,30 +1,22 @@
 import 'package:flutter/material.dart';
 
 class WidgetColors {
-  Color boxShadowColor() {
-    return const Color.fromARGB(255, 158, 158, 158);
-  }
+  Color boxShadowColor() => const Color.fromARGB(255, 158, 158, 158);
 
-  Color iconColor() {
-    return const Color.fromARGB(255, 158, 158, 158);
-  }
+  Color iconColor() => const Color.fromARGB(255, 158, 158, 158);
 
-  Color iconColorMoreDark() {
-    return const Color.fromARGB(255, 90, 90, 100);
-  }
+  Color iconColorMoreDark() => const Color.fromARGB(255, 90, 90, 100);
 
-  Color favoriteIcon() {
-    return Colors.red;
-  }
+  Color favoriteIcon() => Colors.red;
 
-  List<Color> applicationMainTheme() {
-    return [
-      Color.fromARGB(255, 124, 139, 253),
-      Color.fromARGB(255, 123, 75, 253),
-    ];
-  }
+  List<Color> applicationMainTheme() => [
+    Color.fromARGB(255, 124, 139, 253),
+    Color.fromARGB(255, 123, 75, 253),
+  ];
 
-  Color confirmButton() {
-    return Color.fromARGB(255, 97, 126, 255);
-  }
+  Color confirmButton() => Color.fromARGB(255, 97, 126, 255);
+
+  Color selectedShadowColor() => Color.fromARGB(255, 97, 126, 255);
+
+  Color lightTheme() => Colors.white;
 }
