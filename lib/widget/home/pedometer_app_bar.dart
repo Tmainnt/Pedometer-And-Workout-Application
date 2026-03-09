ee83de72fafc590c24f26be0426d02f3f8cef869
Tmainnt <gamerthann@gmail.com>
2026-03-09 18:28:10 +0700

    Add logic to check role and show menu at the left-top of appBar

--- lib/widget/home/pedometer_app_bar.dart
+++ lib/widget/home/pedometer_app_bar.dart
@@ -1,67 +1,123 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pedometer_application/services/firestore_service.dart';
+import 'package:pedometer_application/theme/widget_colors.dart';
+import 'package:pedometer_application/sceen/community/admin/admin_reports_page.dart';
+import 'package:pedometer_application/sceen/community/admin/admin_sanctions_page.dart';
 
 class PedometerAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const PedometerAppBar({super.key});
+  PedometerAppBar({super.key});
+
+  final FirestoreService firestoreService = FirestoreService();
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      flexibleSpace: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 124, 139, 253),
-              Color.fromARGB(255, 123, 75, 253),
+    return FutureBuilder(
+      future: firestoreService.getUserData(),
+      builder: (context, snapshot) {
+        String role = "";
+
+        if (snapshot.hasData) {
+          role = snapshot.data!.role;
+        }
+
+        return AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 124, 139, 253),
+                  Color.fromARGB(255, 123, 75, 253),
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+          ),
+
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text(
+                'Pedometer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                '& Workout',
+                style: TextStyle(
+                  color: Colors.orangeAccent,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ],
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
           ),
-        ),
-      ),
 
-      leading: IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.menu, color: Colors.white, size: 40),
-      ),
+          leading: role == "admin"
+              ? PopupMenuButton<String>(
+                  icon: Icon(
+                    Icons.menu,
+                    color: WidgetColors().lightTheme(),
+                    size: 35,
+                  ),
+                  onSelected: (value) {
+                    if (value == "reports") {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => AdminReportsPage()),
+                      );
+                    } else if (value == "sanctions") {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const AdminSanctionsPage(),
+                        ),
+                      );
+                    }
+                  },
+                  itemBuilder: (context) => [
+                    const PopupMenuItem(
+                      value: "reports",
+                      child: Row(
+                        children: [
+                          Icon(Icons.flag),
+                          SizedBox(width: 10),
+                          Text("ดูรายงาน"),
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem(
+                      value: "sanctions",
+                      child: Row(
+                        children: [
+                          Icon(Icons.gavel),
+                          SizedBox(width: 10),
+                          Text("ประวัติการลงโทษ"),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : null,
 
-      title: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text(
-            'Pedometer',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
+          actions: [
+            IconButton(
+              onPressed: () async {
+                firestoreService.clearUserCache();
+                await FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.logout, color: Colors.white, size: 20),
             ),
-          ),
+          ],
 
-          Text(
-            '& Workout',
-            style: const TextStyle(
-              color: Colors.orangeAccent,
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ],
-      ),
-
-      actions: [
-        IconButton(
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-          },
-          icon: const Icon(Icons.logout, color: Colors.white, size: 20),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(right: 15),
-          child: Icon(Icons.account_circle, color: Colors.white, size: 40),
-        ),
-      ],
-      centerTitle: true,
-      elevation: 0,
+          centerTitle: true,
+          elevation: 0,
+        );
+      },
     );
   }
 
