1cbd23f8d9c2c4138030e917447900abf127135a
Tmainnt <gamerthann@gmail.com>
2026-03-09 22:54:57 +0700

    add new field

--- lib/models/report.dart
+++ lib/models/report.dart
@@ -2,19 +2,25 @@ class ReportModel {
   final String id;
   final String postId;
   final String postOwnerId;
-  final String reporterId;
+  final String reportedUID;
   final String reason;
   final String detail;
   final String status;
+  final String reportedName;
+  final String commentID;
+  final String commentText;
 
   ReportModel({
     required this.id,
     required this.postId,
     required this.postOwnerId,
-    required this.reporterId,
+    required this.reportedUID,
     required this.reason,
     required this.detail,
     required this.status,
+    required this.commentID,
+    required this.reportedName,
+    required this.commentText,
   });
 
   factory ReportModel.fromDoc(doc) {
@@ -24,10 +30,13 @@ class ReportModel {
       id: doc.id,
       postOwnerId: data['postOwnerId'] as String? ?? '',
       postId: data['postId'] as String? ?? '',
-      reporterId: data['reporterId'] as String? ?? '',
+      reportedUID: data['reported_uid'] as String? ?? '',
       detail: data['detail'] as String? ?? '',
       reason: data['reason'] as String? ?? '',
       status: data['status'] as String? ?? 'pending',
+      commentID: data['commentId'] as String? ?? '',
+      commentText: data['commentText'] as String? ?? '',
+      reportedName: data['reported_name'] as String? ?? '',
     );
   }
 }
