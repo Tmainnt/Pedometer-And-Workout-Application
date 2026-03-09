a2db4a4a28fa9f8ab9de65d4e15143293fe1a2ce
Tmainnt <gamerthann@gmail.com>
2026-03-09 22:55:54 +0700

    change AppBar

--- lib/sceen/community/admin/admin_reports_page.dart
+++ lib/sceen/community/admin/admin_reports_page.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:pedometer_application/services/firestore_service.dart';
 import 'package:pedometer_application/models/report.dart';
+import 'package:pedometer_application/theme/widget_colors.dart';
 import 'package:pedometer_application/widget/community/report_card.dart';
 
 class AdminReportsPage extends StatelessWidget {
   final FirestoreService firestoreService = FirestoreService();
+  final WidgetColors widgetColors = WidgetColors();
 
   AdminReportsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("รายงานโพสต์")),
+      appBar: AppBar(
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'รายงานโพสต์',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: widgetColors.applicationMainTheme(),
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close, color: Colors.white),
+        ),
+      ),
       body: StreamBuilder<List<ReportModel>>(
         stream: firestoreService.getReportsStream(),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            print(
-              "🔥 Firebase Error: ${snapshot.error}",
-            ); // สำคัญมาก! บรรทัดนี้จะให้ลิงก์เรา
+            print("Firebase Error: ${snapshot.error}");
             return Padding(
               padding: const EdgeInsets.all(16.0),
               child: Center(
