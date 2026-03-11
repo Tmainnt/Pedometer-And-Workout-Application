93365ae1a45921e5a3669e797f441052540494bb
Tmainnt <gamerthann@gmail.com>
2026-03-11 20:30:20 +0700

    new switch case, make widget can press on show ที่มา

--- lib/sceen/community/notification_page.dart
+++ lib/sceen/community/notification_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pedometer_application/models/community/notification.dart';
 import 'package:pedometer_application/theme/widget_colors.dart';
 import 'package:pedometer_application/services/firestore_service.dart';
+import 'package:pedometer_application/widget/community/create_posts.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
@@ -29,39 +30,68 @@ class NotificationPage extends StatelessWidget {
             itemBuilder: (context, index) {
               final notif = notifications[index];
 
-              return Card(
-                color: notif.isRead ? Colors.white : Colors.grey[100],
-                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                child: ListTile(
-                  title: Text(
-                    _getTitle(notif.type),
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 4),
-                      if (notif.reason.isNotEmpty)
-                        Text("เหตุผล: ${notif.reason}"),
-                      if (notif.detail.isNotEmpty)
-                        Text("รายละเอียด: ${notif.detail}"),
-                      const SizedBox(height: 4),
-                      Text(
-                        "${notif.createTimestamp.day}/${notif.createTimestamp.month}/${notif.createTimestamp.year}",
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey,
+              return GestureDetector(
+                onTap: () async {
+                  final post = await firestoreService.getOnePostById(
+                    notif.postId,
+                  );
+                  if (post != null) {
+                    if (post.postID!.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                            appBar: AppBar(title: const Text('ตรวจสอบโพสต์')),
+                            body: SingleChildScrollView(
+                              physics: const BouncingScrollPhysics(),
+                              child: CreatePosts(
+                                userPost: post,
+                                currentUserRole: 'admin',
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                      );
+                    }
+                  }
+                },
+                child: Card(
+                  color: notif.isRead ? Colors.white : Colors.grey[100],
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 6,
                   ),
-                  trailing: IconButton(
-                    onPressed: () {
-                      firestoreService.deleteNotification(notif.id);
-                    },
-                    icon: Icon(
-                      Icons.cancel,
-                      color: WidgetColors().iconColorMoreDark(),
+                  child: ListTile(
+                    title: Text(
+                      _getTitle(notif.type),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 4),
+                        if (notif.reason.isNotEmpty)
+                          Text("เหตุผล: ${notif.reason}"),
+                        if (notif.detail.isNotEmpty)
+                          Text("รายละเอียด: ${notif.detail}"),
+                        const SizedBox(height: 4),
+                        Text(
+                          "${notif.createTimestamp.day}/${notif.createTimestamp.month}/${notif.createTimestamp.year}",
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      onPressed: () {
+                        firestoreService.deleteNotification(notif.id);
+                      },
+                      icon: Icon(
+                        Icons.cancel,
+                        color: WidgetColors().iconColorMoreDark(),
+                      ),
                     ),
                   ),
                 ),
@@ -79,7 +109,7 @@ class NotificationPage extends StatelessWidget {
         return "โพสต์ของคุณถูกลบ";
       case "reply_comment":
         return "มีการตอบกลับข้อความ";
-      case "comment":
+      case "comment_post":
         return "มีความคิดเห็นใหม่";
       default:
         return "การแจ้งเตือน";
